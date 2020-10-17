a1b66e15bc6c4b52f61e42e97b2414f52a5b7099
HaeSeok Kang <hotbloodturtle@gmail.com>
2020-10-17 13:12:08 +0900

    Add - get data board list, use graphql
    n/a

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,23 @@
 import App, { Container } from 'next/app';
 import React from 'react';
 import GlobalStyles from '../styles/global-styles';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:8000/graphql/',
+  cache: new InMemoryCache(),
+});
 
 class ReactApp extends App<any> {
   public render() {
     const { Component, pageProps } = this.props;
     return (
-      <Container>
-        <GlobalStyles />
-        <Component {...pageProps} />
-      </Container>
+      <ApolloProvider client={client}>
+        <Container>
+          <GlobalStyles />
+          <Component {...pageProps} />
+        </Container>
+      </ApolloProvider>
     );
   }
 }
--- pages/boards/index.tsx
+++ pages/boards/index.tsx
@@ -1,5 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
+import { gql, useQuery } from '@apollo/client';
+
+const GET_BOARDS = gql`
+  {
+    boards {
+      edges {
+        node {
+          id
+          title
+          content
+          createdAt
+          customField
+          photo
+        }
+      }
+    }
+  }
+`;
 
 const Example = styled.div`
   width: 100px;
@@ -7,11 +25,18 @@ const Example = styled.div`
   background-color: red;
 `;
 
-const BoardList: React.FunctionComponent = () => (
-  <>
-    <h1>board list</h1>
-    <Example />
-  </>
-);
+const BoardList: React.FunctionComponent = () => {
+  const { loading, error, data } = useQuery(GET_BOARDS);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error to load!</p>;
+  console.log(data);
+
+  return (
+    <>
+      <h1>board list</h1>
+      <Example />
+    </>
+  );
+};
 
 export default BoardList;
